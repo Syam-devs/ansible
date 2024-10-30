62ea6ed90bd71f5814c977bdde50738967bf6835
syam_devs <syamasundarraodatti@gmail.com>
2024-10-31 00:24:50 +0530

    check code

---
+++ roboshop.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+AMI=ami-0b4f379183e5706b9
+SG_ID=sg-0e4b83efbf93fa4e7
+INSTANCE_TYPE=("mongodb" "redis" "mysql" "rabbit" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=
+DOMAIN_NAME=
+
+for i in "${INSTANCES[@]}"
+DO
+    if [ $i == "mongodb" ] || [ $i == "mysql"] || [ $i == "shipping" ]
+    then 
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --count 1 --instance-type t2.micro --security-group-ids sg-0e4b83efbf93fa4e7
+
+
+    IP_ADRESS=$( aws ec2 run-instances --image-id $ami --instance-type $INSTANCE_TYPE --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${i}}]" --query "Instances[0].PublicIpAddress" --output text )
+    echo "$i: $IP_ADRESS"
+
+aws route53 change-resource-record-sets \
+--hosted-zone-id $ZONE_ID \ 
+--change-batch '
+{
+    "comment"="Creating a record for cognito endpoint",
+    ,"changes"=[{
+    "Action" : "create"
+    ,"ResourceRecords" : {
+        "Name" : "'$i','$DOMAIN_NAME'"
+        ,"Type" : "A"
+        ,"TTL" : 1
+        ,"ResourceRecords" : [{
+        "ResourceValue" : "'$IP_ADRESS'"
+        }]
+    }
+    }]
+}
+'
+done
